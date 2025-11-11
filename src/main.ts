482a9a002f946fe505d1aa11ab62a82711857604
Joshua Mondragon <joshua.mondragon@dragontek.com>
2025-11-11 13:31:17 -0700

    chore: remove unused code

--- src/main.ts
+++ src/main.ts
@@ -47,42 +47,6 @@ const styleSwitcher = new StyleSwitcherControl({
   },
 });
 
-// const geocoderApi:MaplibreGeocoderApi = {
-//   forwardGeocode: async (config: any) => {
-//     const features = [];
-//     try {
-//       const request = `https://nominatim.openstreetmap.org/search?q=${config?.query}&format=geojson&polygon_geojson=1&addressdetails=1`;
-//       const response = await fetch(request);
-//       const geojson = await response.json();
-//       for (const feature of geojson.features) {
-//         const center = [
-//           feature.bbox[0] + (feature.bbox[2] - feature.bbox[0]) / 2,
-//           feature.bbox[1] + (feature.bbox[3] - feature.bbox[1]) / 2,
-//         ];
-//         const point = {
-//           type: "Feature",
-//           geometry: {
-//             type: "Point",
-//             coordinates: center,
-//           },
-//           place_name: feature.properties.display_name,
-//           properties: feature.properties,
-//           text: feature.properties.display_name,
-//           place_type: ["place"],
-//           center,
-//         };
-//         features.push(point);
-//       }
-//     } catch (e) {
-//       console.error(`Failed to forwardGeocode with error: ${e}`);
-//     }
-
-//     return {
-//       features,
-//     };
-//   },
-// };
-
 map.on("load", () => {
   // Add controls to the map
   map.addControl(new maplibregl.NavigationControl(), "top-right");
@@ -102,7 +66,7 @@ map.on("load", () => {
     "bottom-right"
   );
   map.addControl(styleSwitcher, "bottom-left");
-  //map.addControl(new MaplibreGeocoder(geocoderApi, { maplibregl }), "top-left");
-  // Uncomment the following lines to add terrain control and 3D terrain effect
-  map.addControl(new maplibregl.TerrainControl({ source: "terrainSource", exaggeration: 1 }));
+  map.addControl(
+    new maplibregl.TerrainControl({ source: "terrainSource", exaggeration: 1 })
+  );
 });
